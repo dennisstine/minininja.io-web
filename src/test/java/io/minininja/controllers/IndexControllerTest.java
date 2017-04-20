2ac298a68ccf65a0ca626e7b3aff0ccb6ce37e05
Dennis Stine <dennis.stine@gmail.com>
2017-04-19 22:21:10 -0500

    Updating tests.

--- src/test/java/io/minininja/controllers/IndexControllerTest.java
+++ src/test/java/io/minininja/controllers/IndexControllerTest.java
@@ -3,10 +3,14 @@ package io.minininja.controllers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import io.minininja.WebApplication;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -17,6 +21,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  *
  * Created by Dennis on 4/14/2017.
  */
+@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 public class IndexControllerTest {
 
@@ -40,7 +45,7 @@ public class IndexControllerTest {
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
-                .andExpect(model().attribute("fragmentInclude", "home"))
+                .andExpect(model().attribute("fragmentInclude", "fragments/home"))
                 .andExpect(forwardedUrl("/index.html"));
     }
 }
